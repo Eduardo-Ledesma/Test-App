8d53b8c97c5a0663e32f7fbca9abfd6969c00367
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2024-01-31 18:36:54 -0300

    small change

--- src/app/components/CardRender.tsx
+++ src/app/components/CardRender.tsx
@@ -11,10 +11,10 @@ const CardRender = ({ cards }: { cards: Card[] }) => {
     <section className="px-4 mx-auto max-w-[1900px]">
       <div className="max-w-[1600px]">
         <h2 className="text-categoryTitle leading-categoryTitle font-bold text-main-text">Find talent by category</h2>
-        <div className="grid grid-cols-2 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-6">
           { cards && cards.map((card: Card) => (
             <div key={card.id} className="h-64">
-              <Image src={card.picture} alt={card.title} width={385} height={202} className="rounded-lg h-52" />
+              <Image src={card.picture} alt={card.title} width={385} height={202} className="rounded-lg h-52 max-w-96 w-full" />
               <p className="text-gray-medium text-xl p-[10px] font-medium">{card.title}</p>
             </div>
           ))}
